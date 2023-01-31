e7955d4cf6ad58c0c181431cce4cc33b39f06c78
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-01-31 17:04:07 -0500

    react type animation added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,26 @@
+import { TypeAnimation } from "react-type-animation";
+
+const ExampleComponent = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        "One", // Types 'One'
+        1000, // Waits 1s
+        "Two", // Deletes 'One' and types 'Two'
+        2000, // Waits 2s
+        "Two Three", // Types 'Three' without deleting 'Two'
+        () => {
+          console.log("Done typing!"); // Place optional callbacks anywhere in the array
+        },
+      ]}
+      wrapper="div"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: "3.75rem" }}
+    />
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -11,7 +34,7 @@ export default function Home() {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="white"
-          className="w-28"
+          className="w-32 mb-8"
         >
           <path
             strokeLinecap="round"
@@ -19,10 +42,34 @@ export default function Home() {
             d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
           />
         </svg>
-        <div className="flex flex-col h-1/2 text-white mt-4">
-          <h2 className="text-6xl">Juan David</h2>
-          <h3 className="text-2xl">Frontend Developer</h3>
-          <h3 className="text-2xl">Civil Engineer</h3>
+        <div className="flex flex-col text-white ">
+            <TypeAnimation
+              sequence={[
+                "Juan David", // Types 'One'
+                10000,
+                "J", // Waits 1s
+                () => {
+                  console.log("Done typing!"); // Place optional callbacks anywhere in the array
+                },
+              ]}
+              wrapper="h2"
+              cursor={false}
+              repeat={Infinity}
+              className="text-6xl"
+            />
+            <TypeAnimation
+              sequence={[
+                1500,
+                "Frontend Developer", // Types 'One'
+                3000,
+                "Civil Engineer",
+                2000, 
+              ]}
+              wrapper="h3"
+              cursor={true}
+              repeat={Infinity}
+              className="text-3xl mt-2"
+            />
         </div>
       </div>
     </>
