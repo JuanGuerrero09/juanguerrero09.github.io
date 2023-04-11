7a5819c549878bc93f511b0e5ae1415e7a5d7028
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-04-11 15:25:34 -0500

    about me improved

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -32,7 +32,10 @@ export default function About() {
           Hello! My name is Juan David, a 21-year-old civil engineer who is passionate about both civil engineering and web development. I have self-taught myself programming skills and  I'm eager to learn more about software engineering. As someone who enjoys both fields, I'm looking for a place to start my journey as a developer.
           </p>
           <p className="py-2 px-4 text-justify lg:w-2/3">
-          In my free time, I like to work on personal programming projects, learn new technologies, and read technical and non-technical books to expand my knowledge. I am a detail-oriented person who likes to produce new ideas and solving challenges, and loves to collaborate with others to find creative solutions to complex problems. I am excited to bring my unique perspective to any team and work together to create innovative solutions that solve problems.
+          In my free time, I like to work on personal programming projects, learn new technologies, and read technical and non-technical books to expand my knowledge. I am a person who likes to produce new ideas and solving challenges, and loves to collaborate with others to find creative solutions to complex problems.
+          </p>
+          <p className="py-2 px-4 text-justify lg:w-2/3">
+          In programming, I have a solid foundation in web development basics, including HTML, CSS, and JavaScript. I am comfortable using React with TypeScript for more advanced projects and can style them using Tailwind or a Component Library like Bootstrap, DaisyUI, or Chakra UI. I also have a good understanding of Python, which was the first programming language I learned. I used Python in my bachelor's thesis for data analysis and visualization. Recently, I have been expanding my skills in backend development with the MERN stack, which includes MongoDB, Express, and Node.
           </p>
           {/* <button className="btn btn-primary">Get Started</button> */}
         </div>
